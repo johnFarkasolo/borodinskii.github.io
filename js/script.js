27148ca18714aaf0732ade7cbaab42be78a8f7d8
Ivan Yarovyi <farkasolo@mail.ru>
2017-08-04 01:41:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var link = document.querySelector(".login");
+		var link = document.querySelector(".login");
 		var popup = document.querySelector(".modal-content");
 		var close = popup.querySelector(".modal-content-close");
 		var form = popup.querySelector("form");
@@ -39,5 +39,27 @@ var link = document.querySelector(".login");
 				if (popup.classList.contains("modal-content-show")) {
 					popup.classList.remove("modal-content-show");
 				}	
+			}
+		});
+
+		var mapOpen = document.querySelector(".contact-map");
+		var mapPopup = document.querySelector(".modal-content-map");
+		var mapClose = mapPopup.querySelector(".modal-content-close");
+
+		mapOpen.addEventListener("click", function(event) {
+			event.preventDefault();
+			mapPopup.classList.add("modal-content-show");
+		});
+
+		mapClose.addEventListener("click", function(event) {
+			event.preventDefault();
+			mapPopup.classList.remove("modal-content-show");
+		});
+
+		window.addEventListener("keydown", function(event) {
+			if (event.keyCode === 27) {
+				if (mapPopup.classList.contains("modal-content-show")) {
+					mapPopup.classList.remove("modal-content-show");
+				}
 			}
 		});
